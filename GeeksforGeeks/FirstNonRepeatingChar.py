04f1f70eebd947eab3d000fa4a31e0274c3809e3
Himanshu <himnsuk@gmail.com>
2016-09-06 15:21:50 +0530

    Added code string

---
+++ GeeksforGeeks/FirstNonRepeatingChar.py
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+import sys
+
+def firstNonRepeating(str1, divide_size):
+    n = len(str1)
+    if n%divide_size != 0:
+        print "Invalid input string"
+        return
+    k = 0
+    for i in str1:
+        if k%divide_size == 0:
+            print "\n"
+        print i
+        k += 1
+
+str1 = "a_simple_divide_string_quest"
+b = 4
+diviideEqualPart(str1, b)
