aa66d3ddffaf43d8054ace0a59d51d7d0e568b7b
himnsuk <himnsuk@gmail.com>
2021-06-16 22:57:28 +0530

    Rebased

--- Interview/LinkedList/sum_linked_list.py
+++ Interview/LinkedList/sum_linked_list.py
@@ -10,6 +10,13 @@ def create_linked_list_from_number(num):
         ll.add_node_in_end(Node(a))
         num = num // 10
     return ll
+    ll_len = 0
+    while num >= 0:
+        a = num % 10
+        ll.add_node_in_end(Node(a))
+        num = num // 10
+        ll_len += 1
+    return (ll, ll_len)
 
 def sum_linked_list(l1, l2):
     n1 = l1.head
@@ -79,3 +86,4 @@ if __name__ == "__main__":
     if x == num:
         print("Your code working awesome")
         
+    
